9a9c21d64b9976bb9f013de8b446188db8449fcd
hyun <cjh7652@hanmail.net>
2022-04-07 13:18:04 +0900

    rating 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Fruit from './Orange';
 
+
+const fruitLike=[
+  {
+    id: 1,
+    name : 'banana',
+    image : 'https://cdn-icons-png.flaticon.com/512/284/284780.png',
+    rating: 5,
+  },
+  {
+    id:2,
+    name : 'apple',
+    image : 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FYZiGz%2FbtqxdqHsg3G%2FTmrEpyujNbbtNtOsiFpkik%2Fimg.jpg',
+    rating: 4.9,
+  },
+  {
+    id:3,
+    name : 'orang',
+    image : 'https://cdn-icons-png.flaticon.com/512/135/135620.png',
+    rating: 4.7,
+  }
+];
+
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Fruit  fav="banana"/>
-      <Fruit  fav="apple"/>
-      <Fruit  fav="orange"/>
+      {fruitLike.map((dish) => (<Fruit key={dish.id} name={dish.name}  picture={dish.image}/>))}
     </div>
   );
 }
--- src/Orange.js
+++ src/Orange.js
@@ -3,8 +3,15 @@ import React from 'react';
 
 
 
-function Fruit({ fav }){ 
-    return <h1>I like { fav }</h1>;
+function Fruit({ name, picture }){ 
+    return(
+        <div>
+            <h1>I like { name }</h1>;
+            <img src={picture} width="300" alt={name} />
+        </div>
+    ) 
 }
+
+
 
 export default Fruit;
